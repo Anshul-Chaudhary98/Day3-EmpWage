85a41fa5eadec6c61cdf9b244f492ec255b19b5c
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-06-23 21:14:14 +0530

    Add files via upload

---
+++ empWelcomeMsg.java
@@ -0,0 +1,7 @@
+package employewagecomputation;
+
+public class empWelcomeMsg {
+    public static void main(String args[]) {
+      System.out.println("Welcome to the Employee Wage Computation");
+    }
+}
